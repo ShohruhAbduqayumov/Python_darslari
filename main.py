33c557c62dc314db384d97ff04481c7757bb2044
ShohruhAbduqayumov <shohruh1412@gmail.com>
2021-06-30 11:46:38 +0500

    bismillah

---
+++ main.py
@@ -0,0 +1,17 @@
+#i = 2
+
+#while i <= 10:
+ #   print(i)
+  #  i += 2  # i = i + 1
+a=int(input("a="))
+b=int(input("b="))
+i=a
+if a % 2 == 0:
+    pass
+elif a % 2== 1:
+    i=a+1
+    while i<b:
+        print(i)
+    i+=2
+
+
